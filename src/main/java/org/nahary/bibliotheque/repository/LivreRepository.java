f4c6515ea9e98029afdc46f24319257b52b594cf
Kei-Shiro <razafindralambonahary04@gmail.com>
2025-07-03 23:36:36 +0300

    Recherche et filtrage : Ajouter des fonctionnalités pour rechercher des livres ou des emprunts par différents critères (titre, auteur, utilisateur, etc.).

--- src/main/java/org/nahary/bibliotheque/repository/LivreRepository.java
+++ src/main/java/org/nahary/bibliotheque/repository/LivreRepository.java
@@ -4,6 +4,12 @@ import org.nahary.bibliotheque.entity.Livre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LivreRepository extends JpaRepository<Livre, Long> {
+
+    List<Livre> findByTitreContainingIgnoreCase(String titre);
+
+    List<Livre> findByAuteurContainingIgnoreCase(String auteur);
 }
