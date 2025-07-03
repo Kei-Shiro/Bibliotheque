85a8b63299c77fee257a8ec325743fabebc9a65b
Kei-Shiro <razafindralambonahary04@gmail.com>
2025-07-03 23:59:16 +0300

    Gestion des réservations : Permettre aux utilisateurs de réserver des livres lorsqu'ils ne sont pas disponibles.

---
+++ src/main/java/org/nahary/bibliotheque/controller/ReservationController.java
@@ -0,0 +1,42 @@
+package org.nahary.bibliotheque.controller;
+
+import org.nahary.bibliotheque.entity.Reservation;
+import org.nahary.bibliotheque.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservations")
+public class ReservationController {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @GetMapping
+    public List<Reservation> getAllReservations() {
+        return reservationService.getAllReservations();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reservation> getReservationById(@PathVariable Long id) {
+        return reservationService.getReservationById(id);
+    }
+
+    @PostMapping
+    public Reservation createReservation(@RequestBody Reservation reservation) {
+        return reservationService.createReservation(reservation);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Reservation> updateReservation(@PathVariable Long id, @RequestBody Reservation reservation) {
+        return reservationService.updateReservation(id, reservation);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable Long id) {
+        return reservationService.deleteReservation(id);
+    }
+}
